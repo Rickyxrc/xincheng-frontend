270eb38680ca6d28284098a218f1b0acacaf1d95
rickyxrc <rickyxrc@outlook.com>
2022-07-12 13:41:38 +0800

    migrate to vite

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -13,7 +13,6 @@
   <br>
   <el-button type="danger" plain @click="open">真的 不要点我</el-button>
   <el-button type="success" plain @click="open">CLICK ME!</el-button>
-  <el-card>
 </template>
 
 <script lang="ts" setup>
